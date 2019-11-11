67050b7d8f81f5a4fdea7f45312148d77f971651
Ricardo de Souza Bonna <ricardobonna@gmail.com>
2019-11-10 22:40:18 -0300

    Added even more functions to my overhaul experiment

--- src/ForSyDe/Shallow/MoC/Synchronous/OverhaulSY.hs
+++ src/ForSyDe/Shallow/MoC/Synchronous/OverhaulSY.hs
@@ -11,7 +11,7 @@
 -- The synchronous process library overhauled for classified purposes
 -----------------------------------------------------------------------------
 module ForSyDe.Shallow.MoC.Synchronous.OverhaulSY (
-  constant, delta, sat, varSat
+  constant, delta, sat, varSat, safeDiv, folpf, integrate
   ) where
 
 import ForSyDe.Shallow.MoC.Synchronous.Lib
@@ -54,13 +54,39 @@ instance (Real a) => Real (Signal a) where
 constant :: a -> Signal a
 constant = signal . repeat
 
-delta :: (Num a) => Signal a -> Signal Bool -> Signal a
-delta sig = zipWith3SY deltaFunc sig (delaySY 0 sig)
-  where deltaFunc _ _ True = 0
-        deltaFunc s ds False = s - ds
+delta :: (Num a) => Signal Bool -> Signal a -> Signal a
+delta reset sig = zipWith3SY deltaFunc reset sig (delaySY 0 sig)
+  where deltaFunc True _ _ = 0
+        deltaFunc False s ds = s - ds
 
-sat :: (Ord a) => a -> a -> Signal a -> Signal a
-sat x y = mapSY (max x . min y)
+sat :: (Ord a) => (a, a) -> Signal a -> Signal a
+sat (minVal, maxVal) = mapSY (max minVal . min maxVal)
 
 varSat :: (Ord a) => Signal a -> Signal a -> Signal a -> Signal a
 varSat sl sh = max sl . min sh
+
+safeDiv :: (Fractional a, Ord a) => a -> Signal a -> Signal a -> Signal a
+safeDiv e = zipWithSY (safeDivFunc e)
+  where safeDivFunc eps num den
+          | den > 0 = num / safeDen
+          | otherwise = num / negate safeDen
+          where safeDen = max (abs eps) (abs den)
+
+folpf :: (Fractional a) => (a, a) -> Signal Bool -> Signal a -> Signal a
+folpf (period, tau) reset inp = out
+  where out = zipWith4SY folpfFunc reset inp dinp dout
+        dinp = delaySY Abst (mapSY Prst inp)
+        dout = delaySY 0 out
+        a = period / (2*tau+period)
+        b = (2*tau-period) / (2*tau+period)
+        folpfFunc True x _ _ = x
+        folpfFunc _ x Abst _ = x
+        folpfFunc False x (Prst dx) dy = b*dy + a*(x + dx)
+
+integrate :: (Fractional a) => (a, a) -> Signal Bool -> Signal a -> Signal a
+integrate (period, x0) reset inp = out
+  where out = zipWith4SY integrateFunc reset inp dinp dout
+        dinp = delaySY 0 inp
+        dout = delaySY x0 out
+        integrateFunc True _ _ _ = x0
+        integrateFunc False x dx dy = dy + period/2*(x + dx)
